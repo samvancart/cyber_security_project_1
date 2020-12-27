4f76a44e1358cd3ca741f42d7ed67219a272e780
samvancart <sam02cart@gmail.com>
2020-12-27 19:39:41 +0200

    account settings

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import index, signupView, addView, settingsView
+from .views import index, signupView, addView, accountView, deleteView
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', signupView, name='signup'),
     path('add/', addView, name='add'),
-    path('settings/', settingsView, name='settings')
+    path('account/<int:uid>', accountView, name='account'),
+    path('delete/<int:uid>', deleteView, name='delete')
 ]
